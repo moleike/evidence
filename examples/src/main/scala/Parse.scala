2d659459c07d9d25b23217f8d112b55079e205b8
Alexandre Moreno <alexmorenocano@gmail.com>
2024-01-06 10:37:56 +0100

    make Parse effect non-parametric

--- examples/src/main/scala/Parse.scala
+++ examples/src/main/scala/Parse.scala
@@ -6,18 +6,18 @@ import cats.implicits._
 import evidence.effect.NonDet.given
 import cats.Alternative
 
-type Parse[A] = [E, Ans] =>> Parse.Syn[A, E, Ans]
+type Parse = [E, Ans] =>> Parse.Syn[E, Ans]
 
 object Parse:
 
   trait Parser[A] extends (String => Option[(A, String)])
 
-  trait Syn[A, E, Ans]:
-    def satisfy: Op[Parser[A], A, E, Ans]
+  trait Syn[E, Ans]:
+    def satisfy[A]: Op[Parser[A], A, E, Ans]
 
-  def satisfy[E, A](p: Parser[A]): Parse[A] :? E ?=> Eff[E, A] =
-    Eff.perform[Parser[A], A, E, Parse[A]](
-      [EE, Ans] => (_: Parse[A][EE, Ans]).satisfy
+  def satisfy[E, A](p: Parser[A]): Parse :? E ?=> Eff[E, A] =
+    Eff.perform[Parser[A], A, E, Parse](
+      [EE, Ans] => (_: Parse[EE, Ans]).satisfy[A]
     )(p)
 
   def many[E, A](p: Eff[E, A]): NonDet :? E ?=> Eff[E, List[A]] =
@@ -31,12 +31,12 @@ object Parse:
 
   def parse[E, A](
       input: String
-  ): NonDet :? E ?=> Eff[Parse[A] :* E, A] => Eff[E, (A, String)] =
+  ): NonDet :? E ?=> Eff[Parse :* E, A] => Eff[E, (A, String)] =
     Eff.handlerLocalRet(
       input,
       (x: A) => s => (x, s),
-      new Syn[A, Local[String] :* E, (A, String)]:
-        def satisfy: Op[Parser[A], A, Local[String] :* E, (A, String)] =
+      new Syn[Local[String] :* E, (A, String)]:
+        def satisfy[AA]: Op[Parser[AA], AA, Local[String] :* E, (A, String)] =
           Op((p, k) =>
             for
               input <- Local.get
@@ -49,16 +49,16 @@ object Parse:
       _
     )
 
-  def symbol[E](c: Char): Parse[Char] :? E ?=> Eff[E, Char] = satisfy {
+  def symbol[E](c: Char): Parse :? E ?=> Eff[E, Char] = satisfy {
     case s if s.nonEmpty && s.charAt(0) == c => Some(c, s.tail)
     case _                                   => None
   }
 
-  def digit[E]: Parse[Int] :? E ?=> Eff[E, Int] = satisfy {
+  def digit[E]: Parse :? E ?=> Eff[E, Int] = satisfy {
     case s if s.nonEmpty && s.charAt(0).isDigit =>
       Some(s.charAt(0).asDigit, s.tail)
     case _ => None
   }
 
-  def number[E]: (Parse[Int] :? E, NonDet :? E) ?=> Eff[E, Int] =
+  def number[E]: (Parse :? E, NonDet :? E) ?=> Eff[E, Int] =
     many1(digit).map(_.foldLeft(0)(_ * 10 + _))
